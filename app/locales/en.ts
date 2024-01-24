245663cff5c30078fd4717d2eeffeb35cef11ca6
starmorph <dylan@starmorph.com>
2024-01-24 12:03:23 -0700

    space llama

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -137,7 +137,7 @@ const en = {
   },
 
   Welcome: {
-    Title: "Chat LlamaIndex",
+    Title: "Space LLama",
     SubTitle: "Create chat bots that know your data",
     Quote:
       "“This tool has saved me countless hours of work and helped me apply AI features to my work faster than ever before.”",
